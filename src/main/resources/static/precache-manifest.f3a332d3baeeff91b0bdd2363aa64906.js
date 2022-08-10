bd5e5d3507aaa522fbff3c3c28ca136c2bc11b66
Deespider <diptendu.biswas143@gmail.com>
2022-08-10 21:17:19 +0530

    added static files

---
+++ src/main/resources/static/precache-manifest.f3a332d3baeeff91b0bdd2363aa64906.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "827faa01ed96786d423c0fe9e303463e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4276bc99dcfb8e1a1f21",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "405b98af97f5c7d5b2a3",
+    "url": "/static/css/main.941a7d5c.chunk.css"
+  },
+  {
+    "revision": "4276bc99dcfb8e1a1f21",
+    "url": "/static/js/2.adf17f9f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.adf17f9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "405b98af97f5c7d5b2a3",
+    "url": "/static/js/main.41b39b13.chunk.js"
+  },
+  {
+    "revision": "4792b66c7ea268d3b997",
+    "url": "/static/js/runtime-main.d43be887.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
